9a4d5175569810be68404eefb2df060339d22a67
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-07-30 23:35:41 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,14 @@
 class Project
-  attr_accessor :title
+  attr_accessor :title, :backers
 
   def initialize(title)
     @title = title
     @backers = []
+  end
+
+  def add_backer(backer)
+    backers << backer
+    backer.add_backer << self
   end
 
 
